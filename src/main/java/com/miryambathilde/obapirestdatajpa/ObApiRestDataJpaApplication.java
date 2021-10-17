e67f132e816c9f08a39ae938ac0df944c32437c2
Miryam <mbathilde@gmail.com>
2021-10-17 11:00:47 +0200

    initial commit, project created

---
+++ src/main/java/com/miryambathilde/obapirestdatajpa/ObApiRestDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.miryambathilde.obapirestdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObApiRestDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObApiRestDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miryambathilde/obapirestdatajpa/ObApiRestDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miryambathilde.obapirestdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObApiRestDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
