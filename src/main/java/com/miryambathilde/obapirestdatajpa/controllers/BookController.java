db4c97b09970b4b67b20918ae4cc610f1ed8ee5f
Miryam <mbathilde@gmail.com>
2021-10-18 12:24:51 +0200

    get and post methos with path variable as params

--- src/main/java/com/miryambathilde/obapirestdatajpa/controllers/BookController.java
+++ src/main/java/com/miryambathilde/obapirestdatajpa/controllers/BookController.java
@@ -2,10 +2,12 @@ package com.miryambathilde.obapirestdatajpa.controllers;
 
 import com.miryambathilde.obapirestdatajpa.entities.Book;
 import com.miryambathilde.obapirestdatajpa.repositories.BookRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 
@@ -26,10 +28,11 @@ public class BookController {
 
     /**
      * http://localhost:8080/api/books
+     *
      * @return
      */
     @GetMapping("/api/books")
-    public List<Book> findBooks(){
+    public List<Book> findBooks() {
         // recuperar y devolver los libros de la base de datos //
         // tenemos que CREAR el ATRIBUTO + CONTROLADOR de bookrepository para que sea accesible desde el controlador //
         return bookRepository.findAll();
@@ -37,13 +40,37 @@ public class BookController {
     }
 
     // 2. Buscar un solo libro por ID
-    //public Book findOneById(Long id){
 
+    /**
+     * http://localhost:8080/api/books/id
+     * Request
+     * Response
+     * @param id
+     * @return
+     */
+    @GetMapping("/api/books/{id}")
+    // como el parametro id es variable tenemos que poner la notacion @PathVariable
+    // ResponseEntity<Book>
+    public ResponseEntity<Book> findOneById(@PathVariable Long id) {
+        // le ponemos opcional y devuelve un Book
+        Optional<Book> bookOptional = bookRepository.findById(id);
+        // 1. opcion 1
+        if(bookOptional.isPresent())
+            return ResponseEntity.ok(bookOptional.get()); // esto nos devuelve el libro
+        else
+            return ResponseEntity.notFound().build(); // esto responde 404
     }
 
     // 3. Crear un nuevo libro en base de datos
 
+    @PostMapping("/api/books")
+    public Book create(@RequestBody Book book, @RequestHeader HttpHeaders headers){
+        System.out.println(headers.get("User-Agent"));
+        //guardar libro recibido por parametro en la base de datos
+        return bookRepository.save(book);
+    }
+
     // 4. Actualizar un libro existente en base de datos
 
     // 5. Borrar un libro de la base de datos
-
+}
